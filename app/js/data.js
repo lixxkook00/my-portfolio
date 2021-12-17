e174e29e220a537634e62332f13fff2c3dcf07ba
lixxkook00 <linhlinhkook99@gmail.com>
2021-12-17 10:46:13 +0700

    update project

--- app/js/data.js
+++ app/js/data.js
@@ -1,154 +1,169 @@
 const portfolios = [
-    { 
-        name: 'Fresh Mart',
-        img:'./app/img/fresh-mart.png',
-        linkViewPage: 'https://lixxkook00.github.io/fresh-mart/',
-        linkSourceCode: 'https://github.com/lixxkook00/fresh-mart',
-        type: 'project',
+    {
+        name: "Fresh Mart",
+        img: "./app/img/fresh-mart.png",
+        linkViewPage: "https://lixxkook00.github.io/fresh-mart/",
+        linkSourceCode: "https://github.com/lixxkook00/fresh-mart",
+        type: "project",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '62',
+                language: "HTML",
+                color: "#FD732A",
+                percent: "62",
             },
             {
-                language: 'CSS',
-                color: '#F0AA53',
-                percent: '22',
+                language: "CSS",
+                color: "#F0AA53",
+                percent: "22",
             },
             {
-                language: 'JS',
-                color: '#486E34',
-                percent: '16',
-            }
-        ]
+                language: "JS",
+                color: "#486E34",
+                percent: "16",
+            },
+        ],
     },
-    { 
-        name: 'Music Player',
-        img:'./app/img/musicplayer.png',
-        linkViewPage: 'https://lixxkook00.github.io/music-player/',
-        linkSourceCode: 'https://github.com/lixxkook00/music-player',
-        type: 'project',
+    {
+        name: "Music Player",
+        img: "./app/img/musicplayer.png",
+        linkViewPage: "https://lixxkook00.github.io/music-player/",
+        linkSourceCode: "https://github.com/lixxkook00/music-player",
+        type: "project",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '27',
+                language: "HTML",
+                color: "#FD732A",
+                percent: "27",
             },
             {
-                language: 'SCSS',
-                color: '#c4568c',
-                percent: '36',
+                language: "SCSS",
+                color: "#c4568c",
+                percent: "36",
             },
             {
-                language: 'JS',
-                color: '#486E34',
-                percent: '37',
-            }
-        ]
+                language: "JS",
+                color: "#486E34",
+                percent: "37",
+            },
+        ],
     },
-    { 
-        name: 'Shopee',
-        img:'./app/img/shopee.png',
-        linkViewPage: 'https://lixxkook00.github.io/shopee/',
-        linkSourceCode: 'https://github.com/lixxkook00/shopee',
-        type: 'cloneui',
+    {
+        name: "Bounce423",
+        img: "./app/img/bounce423.png",
+        linkViewPage: "https://simmer.io/@lixxkook/bounce-423-final",
+        linkSourceCode: "https://simmer.io/@lixxkook/bounce-423-final",
+        type: "cloneui",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '69',
+                language: "c#",
+                color: "#621192",
+                percent: "100",
             },
-            {
-                language: 'CSS',
-                color: '#F0AA53',
-                percent: '20',
-            },
-            {
-                language: 'JS',
-                color: '#486E34',
-                percent: '11',
-            }
-        ]
+        ],
     },
-    { 
-        name: 'Instagram',
-        img:'./app/img/instagram.png',
-        linkViewPage: 'https://lixxkook00.github.io/clone-ui-instagram/',
-        linkSourceCode: 'https://github.com/lixxkook00/clone-ui-instagram',
-        type: 'cloneui',
+    {
+        name: "Shopee",
+        img: "./app/img/shopee.png",
+        linkViewPage: "https://lixxkook00.github.io/shopee/",
+        linkSourceCode: "https://github.com/lixxkook00/shopee",
+        type: "cloneui",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '71',
+                language: "HTML",
+                color: "#FD732A",
+                percent: "69",
             },
             {
-                language: 'CSS',
-                color: '#F0AA53',
-                percent: '29',
+                language: "CSS",
+                color: "#F0AA53",
+                percent: "20",
             },
             {
-                language: 'JS',
-                color: '#486E34',
-                percent: '0',
-            }
-        ]
+                language: "JS",
+                color: "#486E34",
+                percent: "11",
+            },
+        ],
     },
-    { 
-        name: 'Google Search',
-        img:'./app/img/google-search.png',
-        linkViewPage: 'https://lixxkook00.github.io/clone-ui-google-search/',
-        linkSourceCode: 'https://github.com/lixxkook00/clone-ui-google-search',
-        type: 'cloneui',
+    {
+        name: "Instagram",
+        img: "./app/img/instagram.png",
+        linkViewPage: "https://lixxkook00.github.io/clone-ui-instagram/",
+        linkSourceCode: "https://github.com/lixxkook00/clone-ui-instagram",
+        type: "cloneui",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '57',
+                language: "HTML",
+                color: "#FD732A",
+                percent: "71",
             },
             {
-                language: 'CSS',
-                color: '#F0AA53',
-                percent: '43',
+                language: "CSS",
+                color: "#F0AA53",
+                percent: "29",
             },
             {
-                language: 'JS',
-                color: '#486E34',
-                percent: '0',
-            }
-        ]
+                language: "JS",
+                color: "#486E34",
+                percent: "0",
+            },
+        ],
     },
-    { 
-        name: 'Kookies Store',
-        img:'./app/img/kookies-store.png',
-        linkViewPage: 'https://lixxkook00.github.io/kookies-store/',
-        linkSourceCode: 'https://github.com/lixxkook00/kookies-store',
-        type: 'project',
+    {
+        name: "Google Search",
+        img: "./app/img/google-search.png",
+        linkViewPage: "https://lixxkook00.github.io/clone-ui-google-search/",
+        linkSourceCode: "https://github.com/lixxkook00/clone-ui-google-search",
+        type: "cloneui",
         material: [
             {
-                language: 'HTML',
-                color:'#FD732A',
-                percent: '43',
+                language: "HTML",
+                color: "#FD732A",
+                percent: "57",
             },
             {
-                language: 'CSS',
-                color: '#F0AA53',
-                percent: '47',
+                language: "CSS",
+                color: "#F0AA53",
+                percent: "43",
             },
             {
-                language: 'JS',
-                color: '#486E34',
-                percent: '10',
-            }
-        ]
+                language: "JS",
+                color: "#486E34",
+                percent: "0",
+            },
+        ],
     },
-    
-]
-const renderPortfolios = (portfolios,element,callback) => {
-    const portfolioContent = portfolios.map((item) => {
-        const processPercent = item.material.map(language => {
-            return `
+    {
+        name: "Kookies Store",
+        img: "./app/img/kookies-store.png",
+        linkViewPage: "https://lixxkook00.github.io/kookies-store/",
+        linkSourceCode: "https://github.com/lixxkook00/kookies-store",
+        type: "project",
+        material: [
+            {
+                language: "HTML",
+                color: "#FD732A",
+                percent: "43",
+            },
+            {
+                language: "CSS",
+                color: "#F0AA53",
+                percent: "47",
+            },
+            {
+                language: "JS",
+                color: "#486E34",
+                percent: "10",
+            },
+        ],
+    },
+];
+const renderPortfolios = (portfolios, element, callback) => {
+    const portfolioContent = portfolios
+        .map((item) => {
+            const processPercent = item.material
+                .map((language) => {
+                    return `
                     <span data-percent="${language.percent}" 
                           data-color="${language.color}" 
                           class="process__item 
@@ -156,9 +171,10 @@ const renderPortfolios = (portfolios,element,callback) => {
                           >
                            ${language.language}
                     </span>
-                `
-        }).join("")
-        return `
+                `;
+                })
+                .join("");
+            return `
                 <div class=" col-xl-4 col-lg-4 col-md-6 col-sm-12">
                     <div class="portfolio__item">
                         <div class="portfolio__item-name">
@@ -182,10 +198,11 @@ const renderPortfolios = (portfolios,element,callback) => {
                         </div>
                     </div>
                 </div>
-            `
-    }).join('')
-    
+            `;
+        })
+        .join("");
+
     element.innerHTML = portfolioContent;
-}
+};
 export default portfolios;
-export {renderPortfolios}
+export { renderPortfolios };
